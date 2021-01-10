b6e9e777ecea5633259e0b9589da7f89b1c6e236
npadovani <n.padovani@betstarters.com>
2021-01-10 18:29:33 +0100

    feature(): updated swagger gen with authorization

--- JWTAuthorization/Startup.cs
+++ JWTAuthorization/Startup.cs
@@ -67,9 +67,41 @@ namespace JWTAuthorization
 				};
 			});
 
-			services.AddSwaggerGen(c =>
+			services.AddSwaggerGen(swagger =>
 			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "JWTAuthorization", Version = "v1" });
+				//This is to generate the Default UI of Swagger Documentation
+				swagger.SwaggerDoc("v1", new OpenApiInfo
+				{
+					Version = "v1",
+					Title = "JWTAuthorization",
+					Description = "Authorization in ASP.NET 5 with JWT and Swagger"
+				});
+
+				// To Enable authorization using Swagger (JWT)
+				swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+				{
+					Name = "Authorization",
+					Type = SecuritySchemeType.ApiKey,
+					Scheme = "Bearer",
+					BearerFormat = "JWT",
+					In = ParameterLocation.Header,
+					Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
+				});
+
+				swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						  new OpenApiSecurityScheme
+						  {
+								Reference = new OpenApiReference
+								{
+									  Type = ReferenceType.SecurityScheme,
+									  Id = "Bearer"
+								}
+						  },
+						  new string[]{ }
+					}
+				});
 			});
 		}
 	}
